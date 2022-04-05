cb7ad25f4012f596cbf09d52a8e0cce130e9e637
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-04-05 20:55:16 -0300

    Create Pessoa.cs

---
+++ Aula 5/Herança/Exercícios/Ex1/Pessoa.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Pessoa
+    {
+        private string nome;
+        private char sexo;
+
+        public Pessoa(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public Pessoa(char sexo)
+        {
+            this.sexo = sexo;
+        }
+
+        public Pessoa()
+        {
+
+        }
+
+        public string GetNome()
+        {
+            return this.nome;
+        }
+
+        public char GetSexo()
+        {
+            return this.sexo;
+        }
+
+        public void SetNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public void SetSexo(char sexo)
+        {
+            this.sexo = sexo;
+        }
+    }
+}
