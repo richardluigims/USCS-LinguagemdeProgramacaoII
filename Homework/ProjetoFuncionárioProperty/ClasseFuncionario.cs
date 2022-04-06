a006d18d3f664a1484adfacc130cc36da7b35a41
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-04-06 12:27:10 -0300

    Create ClasseFuncionario.cs

---
+++ Homework/ProjetoFuncionárioProperty/ClasseFuncionario.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Funcionario
+    {
+        public string nome { get; set; }
+        public int matricula { get; set; }
+        public double salario { get; set; }
+    }
+}
