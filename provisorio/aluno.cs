736eb450c59c33674163167b18b9cc6fc6bb00da
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-04-17 22:09:13 -0300

    Create aluno.cs

---
+++ provisorio/aluno.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Aluno : Pessoa
+    {
+        public int numChamada { get; set; }
+        public double p1 { get; set; }
+        public double p2 { get; set; }
+    }
+}
