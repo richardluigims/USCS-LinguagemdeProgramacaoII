10e1137f66c725d81034f385301c216b4a14fd62
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-04-17 22:08:27 -0300

    Create Pessoa.cs

---
+++ provisorio/Pessoa.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Pessoa
+    {
+        public string nome { get; set; }
+        public int sexo { get; set; }
+    }
+}
