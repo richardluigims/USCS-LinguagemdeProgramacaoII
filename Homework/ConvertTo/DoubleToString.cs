9255b398a90cc9a4bedd2f18b5615bc078c4cfc4
Richard Luigi Mantoan Stopa <richardluigims@gmail.com>
2022-04-19 13:10:18 -0300

    Create DoubleToString.cs

---
+++ Homework/ConvertTo/DoubleToString.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 9.5;
+            string troca;
+
+            troca = Convert.ToString(x);
+            
+            Console.WriteLine("{0}", troca);
+        }
+    }
+}
